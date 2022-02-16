e1aecb1a45e04374f67c76927055519828dfb620
webobite <singhsubham510@gmail.com>
2022-02-16 23:37:55 +0530

    SUBHAM | Structured the Component directory

--- src/components/HeaderComponent/Header.tsx
+++ src/components/HeaderComponent/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Button } from './Button';
+import { Button } from '../ButtonComponent/Button';
 import './header.css';
 
 type User = {
--- src/components/PageComponent/Page.tsx
+++ src/components/PageComponent/Page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Header } from './Header';
+import { Header } from '../HeaderComponent/Header';
 import './page.css';
 
 type User = {

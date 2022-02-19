0a59276c3e9c846ddb8ab0d9c21d34b1b892dc2c
webobite <singhsubham510@gmail.com>
2022-02-19 11:50:06 +0530

    SUBHAM | Implemented the Icon Component with the svg

---
+++ src/components/BrowkUIKit/Icon/index.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import iconPath from "../../../assets/images/icons/IconLib";
+
+interface IIcon {
+  /**
+   * Size of Icons (Width and Height) in px
+   */
+  size: number | undefined;
+  /**
+   * With Size properties respective units in css
+   *  for ex : icon width and height 
+   *      width : 2px
+   *      height: 2px
+   */
+  unit: "px" | "em" | "pt" | "pc" | "em" | undefined;
+  /**
+   * ViewBox properties of the svg Icon
+   *  for example : 
+   *      viewBox="0 0 24 24"
+   */
+  viewBox: "" | undefined;
+  /**
+   * Color of Icon
+   */
+  color: string | undefined;
+  /**
+   * Predefined Icons in provided in libs
+   *    More icon support will be added in future
+   */
+  icon: "loader_xs" | "checked_xs";
+  /**
+   * custom style with className
+   */
+  className: "" | undefined;
+  /**
+   * Custom inline styles to Icon Component
+   */
+  style: React.CSSProperties;
+}
+
+const defaultStyles = { display: "inline-block", verticalAlign: "middle" };
+
+const Icon = (props: IIcon) => {
+  const { size, unit, viewBox, color, icon, className, style } = props;
+  const styles = { ...defaultStyles, ...style };
+
+  return (
+    <svg
+      className={className}
+      style={styles}
+      viewBox={viewBox}
+      width={`${size}${unit}`}
+      height={`${size}${unit}`}
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+    >
+      <path fill={color} d={iconPath[icon]} />
+    </svg>
+  );
+};
+
+export default Icon;
